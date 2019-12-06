9a3a6c558cb0ac5f42076e7aaacf0ad2644b1821
Hodor <51384815+Hohohodor1@users.noreply.github.com>
2019-12-06 14:30:38 +0200

    ssssss

--- src/components/MainWeapons.vue
+++ src/components/MainWeapons.vue
@@ -3,7 +3,7 @@
   <header>  <div class="zbroya">Інформація про втрачену зброю</div>  </header>
 <nav class="top-menu">
   <ul class="menu-main">
-    <!-- <li><router-link to="/">Пошук</router-link></li> -->
+    <li><router-link to="/">Пошук</router-link></li>
           <li><router-link to="/YearsChart">Графік років</router-link></li>
               <li><router-link to="/CaliberChart">Графіки калибров</router-link></li>
                  <li><router-link to="/Info">Стаття</router-link></li>
